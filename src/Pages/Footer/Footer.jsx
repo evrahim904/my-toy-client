17b4a535aa7e996bcc5ae32359d096adef4c46fd
unknown <you@example.com>
2023-05-22 22:44:15 +0600

    footer has been updated

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,28 +1,48 @@
-import { Link } from "react-router-dom";
-import logo from '../../assets/logo.jpg'
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import logo from "../../assets/logo.jpg";
 
 const Footer = () => {
-    return (
-        <footer className="footer p-10 bg-neutral text-neutral-content">
-        <div>
-         <img className="rounded-lg" src={logo} alt="" />
-        </div> 
-        <div>
-          <span className="footer-title">Company</span> 
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <Link to="/"> Home</Link>
-                  
-                  <Link to="blog"> Blog</Link>
-        </div> 
-        <div>
-          <span className="footer-title">Legal</span> 
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+  return (
+    <footer className=" footer bg-neutral py-8 p-10 text-neutral-content">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <div className="flex items-center">
+            <img src={logo} alt="Logo" className="w-10 h-10 mr-2" />
+            <h3 className="text-lg font-bold">tiny speedstars</h3>
+          </div>
+          <div className="text-sm">
+            <p>Contact Information:</p>
+            <p>Email: toycar@gmail.com</p>
+            <p>Phone: +1234567890</p>
+          </div>
+          <div className="text-sm">
+            <p>Follow Us:</p>
+            <div className="flex">
+              <a href="#" className="mr-2">
+                <FaFacebook className="text-xl" />
+              </a>
+              <a href="#" className="mr-2">
+                <FaTwitter className="text-xl" />
+              </a>
+              <a href="#">
+                <FaInstagram className="text-xl" />
+              </a>
+            </div>
+          </div>
+          <div className="text-sm">
+            <p>Address:</p>
+            <p>1234 Street, dhaka, bangladesh</p>
+          </div>
         </div>
-      </footer>
-    );
+        <div className="mt-4 text-center">
+          <p className="text-sm text-gray-500">
+            &copy; {new Date().getFullYear()} Your Website Name. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
