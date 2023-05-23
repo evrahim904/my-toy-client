aa3972e6f1bcb79c1e013e0c4072b02f484ac236
unknown <you@example.com>
2023-05-23 15:08:24 +0600

    mobile resonsive has been completed

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -3,30 +3,79 @@ import slider2 from '../../assets/slider2.jpg'
 import slider3 from '../../assets/slider3.jpg'
 import slider4 from '../../assets/slider4.jpg'
 const Banner = () => {
-    return (
-        <div>
-            <div className="carousel w-full ">
-  <div id="item1" className="carousel-item w-full rounded-lg">
-    <img src={slider1}className="w-full" />
-  </div> 
-  <div id="item2" className="carousel-item w-full">
-    <img src={slider2} className="w-full " />
-  </div> 
-  <div id="item3" className="carousel-item w-full ">
-    <img src={slider3} className="w-full" />
-  </div> 
-  <div id="item4" className="carousel-item w-full">
-    <img src={slider4}className="w-full" />
-  </div>
-</div> 
-<div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-  <a href="#item4" className="btn btn-xs">4</a>
-</div>
+  return (
+    <div className="carousel w-full h-[700px]">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={slider1} className="w-full" />
+        
+        <div className="absolute h-full left-0 right-5 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className= ' absolute text-white space-y-6 w-1/3 pl-4 top-1/2'>
+            <h2 className='text-5xl font-bold'>Get Your Toy Car Collection</h2>
+            <p>
+
+              Unleash speed and style with our toy car collection. Meticulously crafted miniatures, perfect for collectors and enthusiasts. Fuel your passion and ignite your imagination today!
+            </p>
+          </div>
         </div>
-    );
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-3 ">
+          <a href="#slide4" className="btn btn-circle">❮</a>
+          <a href="#slide2" className="btn btn-circle">❯</a>
+        </div>
+
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={slider2} className="w-full" />
+        
+        <div className="absolute h-full left-0 right-5 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className= ' absolute text-white space-y-6 w-1/3 pl-4 top-1/2'>
+            <h2 className='text-5xl font-bold'>Get Your Toy Car Collection</h2>
+            <p>
+
+              Unleash speed and style with our toy car collection. Meticulously crafted miniatures, perfect for collectors and enthusiasts. Fuel your passion and ignite your imagination today!
+            </p>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-3 ">
+          <a href="#slide1" className="btn btn-circle">❮</a>
+          <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={slider3} className="w-full" />
+       
+        <div className="absolute h-full left-0 right-5 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className= ' absolute text-white space-y-6 w-1/3 pl-4 top-1/2'>
+            <h2 className='text-5xl font-bold'>Get Your Toy Car Collection</h2>
+            <p>
+
+              Unleash speed and style with our toy car collection. Meticulously crafted miniatures, perfect for collectors and enthusiasts. Fuel your passion and ignite your imagination today!
+            </p>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-3 ">
+          <a href="#slide2" className="btn btn-circle">❮</a>
+          <a href="#slide4" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={slider4} className="w-full" />
+       
+        <div className="absolute h-full left-0 right-5 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className= ' absolute text-white space-y-6 w-1/3 pl-4 top-1/2'>
+            <h2 className='text-5xl font-bold'>Get Your Toy Car Collection</h2>
+            <p>
+
+              Unleash speed and style with our toy car collection. Meticulously crafted miniatures, perfect for collectors and enthusiasts. Fuel your passion and ignite your imagination today!
+            </p>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-3 ">
+          <a href="#slide3" className="btn btn-circle">❮</a>
+          <a href="#slide1" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
